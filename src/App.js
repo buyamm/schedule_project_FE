0610c4fd0ec864780bae558ce3f685d355e9f9b7
buyamm <truongcongly139@gmail.com>
2026-07-28 10:03:25 +0700

    push to repo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SubjectSelector from './components/SubjectSelector';
 import ScheduleDisplay from './components/ScheduleDisplay';
